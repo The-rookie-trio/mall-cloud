1d86f8a45b11378da40fcb595eb3143c40fb05c9
exphuhong <1042331526@qq.com>
2021-04-21 10:23:21 +0800

    去掉无用模块

--- order-server/src/main/java/com/hujingli/micro/dao/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hujingli.micro.dao;
-
-import org.trt.micro.common.bean.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, String> {
-
-}
--- stock-server/src/main/java/com/hujingli/micro/dao/StockRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hujingli.micro.dao;
-
-import org.trt.micro.common.bean.Stock;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StockRepository extends JpaRepository<Stock, String> {
-
-    Stock findStockByGoodsCode(String goodsCode);
-}
